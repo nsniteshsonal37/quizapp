2347a00e922a9bbbe57857b5b5bc59555ba97a44
simran-gera <simran99.gera@gmail.com>
2021-12-08 16:44:24 +0530

    resolved conflicts

--- models.py
+++ models.py
@@ -22,7 +22,6 @@ class Admins(db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(500), unique = False, nullable=False)
 
-<<<<<<< HEAD
 class questions(db.Model):
     __tablename__="question"
     qid = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
@@ -36,15 +35,3 @@ class questions(db.Model):
     type = db.Column(db.String, nullable=False)
     bcol = db.Column(db.String, nullable=True) 
     marks = db.Column(db.Numeric, nullable=False) 
-=======
-class Questions(db.Model):
-    __tablename__ ='questions'
-    # for true/false questions insert option1 as true and 2 as false.
-    id = db.Column(db.Integer, primary_key = True, unique=True, nullable=False)
-    question = db.Column(db.String(500), unique=False, nullable=False)
-    option1 = db.Column(db.String(200), unique=False, nullable=False)
-    option2 = db.Column(db.String(200), unique=False, nullable=False)
-    option3 = db.Column(db.String(200), unique=False, nullable=True)
-    option4 = db.Column(db.String(200), unique=False, nullable=True)
-    answer = db.Column(db.Integer, unique = False, nullable=False)
->>>>>>> 4efd5d02509fcfbb17d308b99362591101702aa9
